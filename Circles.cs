760f7250bf570b0d049e5005823a7642209ca81e
DuongNguyen999 <165481404+DuongNguyen999@users.noreply.github.com>
2024-04-16 21:44:34 +0900

    Add
    Add

---
+++ Circles.cs
@@ -0,0 +1,22 @@
+class Circles 
+{
+    float radius;
+    public Circles()
+    {
+        Console.WriteLine("khong ban kinh khoi tao");
+
+    }
+    public Circles(float radius)
+    {
+        this.radius = radius;
+    }
+    public float AreaCircle()
+    {
+        float localRadius = 3.14f*radius*radius;
+        return localRadius;
+    }
+    public float PerimeteCircle()
+    {
+        return 3.14f*2*radius;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+    //
+        Circles myCircleOne = new Circles(12);
+        Console.WriteLine("Dien tich duong tron la{0}",myCircleOne.AreaCircle());
+        Console.WriteLine("Chu vi duong tron la{0}",myCircleOne.PerimeteCircle());
+        //
+    Circles myCircleTwo = new Circles(15);
+        Console.WriteLine("Dien tich duong tron la{0}",myCircleTwo.AreaCircle());
+        Console.WriteLine("Chu vi duong tron la{0}",myCircleTwo.PerimeteCircle());
+        //
+        
+        //
+    Inventory myInventory = new Inventory();
+
+    foreach (var item in myInventory.myItems)
+    {
+        Console.Write(item.itemName+",");
+        Console.Write(item.attack+",");
+        Console.Write(item.health+",");
+        Console.WriteLine("");
+    }
+    }  
+}
